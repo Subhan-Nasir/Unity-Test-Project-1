92e70213ee2d08a18f61fc265f69c2ef6504e4b8
Subhan <zcemsna@ucl.ac.uk>
2021-11-02 10:35:26 +0000

    First commit

---
+++ Assets/CarCamera/CarCam.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+//https://www.youtube.com/watch?v=VyOGduSxyqk 
+
+public class CarCam : MonoBehaviour
+{
+    Transform rootNode;
+    Transform carCam;
+    Transform car;
+    Rigidbody carPhysics;
+
+    [Tooltip("If car speed is below this value, then the camera will default to looking forwards (Default value = 1.0).")]
+    public float rotationThreshold = 1f; // Default value 1f
+    
+    [Tooltip("How closely the camera follows the car's position. The lower the value, the more the camera will lag behind (Default value = 10.0).")]
+    public float cameraStickiness = 10.0f; // Default value 10.0f
+    
+    [Tooltip("How closely the camera matches the car's velocity vector. The lower the value, the smoother the camera rotations, but too much results in not being able to see where you're going (Default value = 5.0).")]
+    public float cameraRotationSpeed = 5.0f; // Default value 5.0f
+
+    void Awake()
+    {
+        carCam = Camera.main.GetComponent<Transform>();
+        rootNode = GetComponent<Transform>();
+        car = rootNode.parent.GetComponent<Transform>();
+        carPhysics = car.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        // Detach the camera so that it can move freely on its own.
+        rootNode.parent = null;
+    }
+
+    void FixedUpdate()
+    {
+        Quaternion look;
+
+        // Moves the camera to match the car's position.
+        rootNode.position = Vector3.Lerp(rootNode.position, car.position, cameraStickiness * Time.fixedDeltaTime);
+
+        // If the car isn't moving, default to looking forwards. Prevents camera from freaking out with a zero velocity getting put into a Quaternion.LookRotation
+        if (carPhysics.velocity.magnitude < rotationThreshold)
+            look = Quaternion.LookRotation(car.forward);
+        else
+            look = Quaternion.LookRotation(carPhysics.velocity.normalized);
+        
+        // Rotate the camera towards the velocity vector.
+        look = Quaternion.Slerp(rootNode.rotation, look, cameraRotationSpeed * Time.fixedDeltaTime);                
+        rootNode.rotation = look;
+    }
+}
---
+++ Assets/drive_script.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// Code adapted from:
+// https://www.youtube.com/watch?v=x0LUiE0dxP0&list=PLcbsEpz1iFyjjddSqLxnnGSJthfCcmsav
+// https://www.youtube.com/watch?v=mnAEeE3FcvA&list=PLi-ukGVOag_2oMT2_Nmq_EbqOaS-s7xr6
+// https://www.youtube.com/watch?v=BwL3Dm8GJtQ
+// 
+
+public class drive_script : MonoBehaviour
+{
+
+    public WheelCollider[] wheel_colliders;
+    public GameObject[] wheels;
+
+    [Header("Drivetrain")]
+    public float max_torque = 200;
+    public bool all_wheel_drive = true;
+
+    [Header("Standard Steering")]
+    public float max_steer_angle = 30;
+
+    [Header("Ackermann steering")]
+    public bool Ackermann_Steering = true;
+    public float wheel_base = 1.5f;
+    public float rear_track = 1.085f;
+    public float turn_radius = 3.14f;
+
+
+    [Header("Anti-roll bars")]
+    public bool enable_anti_roll_bars = true;
+    public float AntiRoll = 5000.0f;
+
+
+
+    private float steer_angle_left;
+    private float steer_angle_right;
+
+    private float accel;
+    private float steer;
+
+    private Rigidbody car;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start(){
+        car = GetComponent<Rigidbody> ();      
+        
+    }
+
+
+    // FixedUpdate is called once per frame
+    void FixedUpdate(){
+
+        accel = Input.GetAxis("Vertical");
+        accel = Mathf.Clamp(accel, -1,1);
+
+        steer = Input.GetAxis("Horizontal");
+        steer = Mathf.Clamp(steer, -1,1);      
+
+        
+        // Calls the function to drive the car
+        drive(accel,steer);
+
+    }
+
+    // Function to drive the the car (accerlerate/decelerate and steer)
+    void drive(float accel, float steer){  
+
+        float torque = accel*max_torque;
+       
+        // Apply torque to wheels based on all wheel drive or rear wheel drive
+        if (all_wheel_drive == true){
+
+            //Apply torque to all 4 wheels
+            for(int i = 0; i<4; i++){
+            wheel_colliders[i].motorTorque = torque; 
+            }
+
+        }
+        else{
+
+            // Apply torque to only the rear wheels
+            wheel_colliders[2].motorTorque = torque;
+            wheel_colliders[3].motorTorque = torque;
+        }
+        
+        // Calls the steering function to apply steering for the given input
+        apply_steering(steer);
+
+
+        // Applies force from the anti-roll bars if they are enabled.
+        if (enable_anti_roll_bars == true){
+            WheelCollider left_wheel = wheel_colliders[2];
+            WheelCollider right_wheel = wheel_colliders[3];
+            apply_anti_roll_bars(left_wheel, right_wheel);
+        }
+
+
+        // Makes the 3D model of the wheels spin;
+        for(int i = 0; i<4; i++){        
+            
+            Quaternion q; 
+            Vector3 pos;
+            wheel_colliders[i].GetWorldPose(out pos, out q);
+            wheels[i].transform.position = pos;
+            wheels[i].transform.rotation = q;
+
+        }
+        
+
+    }
+
+
+    // Function for steering the car.
+    void apply_steering(float steer){
+
+        // Applies Ackermann sterring if it is enabled.
+        if(Ackermann_Steering){
+            //Steering right
+            if(steer > 0){
+                steer_angle_left = Mathf.Rad2Deg * Mathf.Atan(wheel_base / (turn_radius + (rear_track/2))) * steer;
+                steer_angle_right = Mathf.Rad2Deg * Mathf.Atan(wheel_base / (turn_radius - (rear_track/2))) * steer;
+
+            }//Steering left            
+            else if (steer < 0){
+                steer_angle_left = Mathf.Rad2Deg * Mathf.Atan(wheel_base / (turn_radius - (rear_track/2))) * steer;
+                steer_angle_right = Mathf.Rad2Deg * Mathf.Atan(wheel_base / (turn_radius + (rear_track/2))) * steer;
+                
+
+            } // Not steering
+            else{
+                steer_angle_left = 0;
+                steer_angle_right = 0;
+
+            }
+            wheel_colliders[0].steerAngle = steer_angle_left;
+            wheel_colliders[1].steerAngle = steer_angle_right;
+        }
+        // If Ackermann steering is disabled, both wheels have the same steer angle.
+        else{
+            wheel_colliders[0].steerAngle = steer * max_steer_angle;
+            wheel_colliders[1].steerAngle = steer * max_steer_angle;
+
+        }
+    }
+
+
+
+    // Function to apply forces from the anti-roll bars.
+    void apply_anti_roll_bars(WheelCollider wheel_left, WheelCollider wheel_right){
+
+        WheelHit hit;
+		float travelL = 1.0f;
+		float travelR = 1.0f;
+
+
+		bool groundedL = wheel_left.GetGroundHit (out hit);
+		if (groundedL) {
+			travelL = (-wheel_left.transform.InverseTransformPoint (hit.point).y - wheel_left.radius) / wheel_left.suspensionDistance;
+		}
+
+		bool groundedR = wheel_right.GetGroundHit (out hit);
+		if (groundedR) {
+			travelR = (-wheel_right.transform.InverseTransformPoint (hit.point).y - wheel_right.radius) / wheel_right.suspensionDistance;
+		}
+
+		float antiRollForce = (travelL - travelR) * AntiRoll;
+
+		if (groundedL)
+			car.AddForceAtPosition (wheel_left.transform.up * -antiRollForce, wheel_left.transform.position);
+
+		if (groundedR)
+			car.AddForceAtPosition (wheel_right.transform.up * antiRollForce, wheel_right.transform.position);
+
+    }
+}
