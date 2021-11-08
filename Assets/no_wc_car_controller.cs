7dd8076751d1a58d011811d4605bfe6361e22483
Subhan <zcemsna@ucl.ac.uk>
2021-11-08 18:25:47 +0000

    Fixed raycast suspension

--- Assets/no_wc_car_controller.cs
+++ Assets/no_wc_car_controller.cs
@@ -13,38 +13,50 @@ public class no_wc_car_controller : MonoBehaviour{
     public float restLength;
     public float springTravel;
     public float springStiffness;
+    public float dampingCoefficient;
     
     private float minLength;
     private float maxLength;
     private float[] springLength = new float[4];
+    private float previousLength;
+    private float springVelocity;
 
     private float springForce;
+    private float damperForce;
     private Vector3 suspensionForce;
+    
 
     [Header("Wheel")]
     public float wheelRadius;
 
     void Start(){
         
-        // rb = GetComponent<Rigidbody>();
         minLength = restLength - springTravel;        
         maxLength = restLength + springTravel;
+        for (int i = 0; i<4; i++){
+            springLength[i] = restLength;
+        }
 
     }
 
     void FixedUpdate(){
-        Debug.Log($"Max length = {maxLength}, Min length = {minLength}");
+        
 
 
         for(int i = 0; i<springs.Count; i++){
             
             bool contact = Physics.Raycast(springs[i].transform.position, -transform.up, out RaycastHit hit, maxLength + wheelRadius);
-            // Debug.Log(contact);
             if(contact){
+
+                previousLength = springLength[i];
                 springLength[i] = hit.distance - wheelRadius;
+                springLength[i] = Mathf.Clamp(springLength[i], minLength, maxLength);
+
+                springVelocity = (springLength[i] - previousLength)/Time.fixedDeltaTime;
 
                 springForce = springStiffness * (restLength - springLength[i]);
-                suspensionForce = springForce * hit.normal;
+                damperForce = dampingCoefficient * springVelocity;
+                suspensionForce = (springForce - damperForce) * hit.normal;
 
                 rb.AddForceAtPosition(suspensionForce, hit.point);
 
@@ -64,18 +76,24 @@ public class no_wc_car_controller : MonoBehaviour{
         for(int i = 0; i < springs.Count; i++){
         
 
-            Vector3 direction = -transform.up *(springLength[i]+wheelRadius);
-            Gizmos.DrawRay(springs[i].transform.position, direction);
+            // Vector3 direction = -transform.up *(springLength[i]+wheelRadius);
+            // Gizmos.DrawRay(springs[i].transform.position, direction);
 
-            Ray ray = new Ray(springs[i].transform.position, -transform.up);
 
+            
+
+            Ray ray = new Ray(springs[i].transform.position, -transform.up);           
+            Gizmos.color = Color.white;
+            Gizmos.DrawSphere(springs[i].transform.position, 0.1f);
+
+            Gizmos.DrawWireSphere( transform.TransformPoint(rb.centerOfMass),0.1f);
             Gizmos.color = Color.blue;
+                        
             Gizmos.DrawLine(ray.origin, -springLength[i] * transform.up + springs[i].transform.position);
 
             Gizmos.color = Color.white;
-            Gizmos.DrawLine(-restLength * transform.up + springs[i].transform.position, -restLength * transform.up + springs[i].transform.position + transform.up * -wheelRadius);
-
-
+            Gizmos.DrawLine(-springLength[i] * transform.up + springs[i].transform.position, -springLength[i] * transform.up + springs[i].transform.position + transform.up * -wheelRadius);
+            Gizmos.DrawSphere((springs[i].transform.position) + (springLength[i]+wheelRadius)*(-transform.up),0.1f);
         
             // Gizmos.color = Color.white;
             // Gizmos.DrawRay(spring.transform.position, -transform.up * (maxLength + wheelRadius));
