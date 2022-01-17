a5e043a2e78125ea71c06d76bac297eafbae37e7
Ramy Haddad <ramy.haddad.18@ucl.ac.uk>
2022-01-17 15:12:29 +0000

    Added UI

---
+++ Assets/VehicleUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VehicleUI : MonoBehaviour
+{
+    public Rigidbody target;
+
+    [Header("UI")]
+    public Text speedLabel; // The label that displays the speed;
+    public Text accelerationLabel;
+    private float speed = 0.0f;
+    private float acceleration = 0.0f;
+    private float lastVelocity;
+    private void Update()
+    {
+        // 3.6f to convert in kilometers
+        // ** The speed must be clamped by the car controller **
+        speed = target.velocity.magnitude;
+        acceleration = (speed - lastVelocity) / Time.deltaTime;
+        lastVelocity = speed;
+        
+        
+
+        if (speedLabel != null)
+            speedLabel.text = " Vehicle Speed:" + Math.Round(speed, 2) + "m/s";
+
+       if (accelerationLabel != null)
+            accelerationLabel.text = "Acceleration:"+ Math.Round(acceleration,2)+ "m/s^2";              
+    }
+}
